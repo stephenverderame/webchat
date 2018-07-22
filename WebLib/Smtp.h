34158cd012e0becb3b56d721c4fa7e39b510c79a
Stephen Verderame <stephenverderame@gmail.com>
2018-07-22 14:22:26 -0400

    Added project files

--- WebLib/Smtp.h
+++ WebLib/Smtp.h
@@ -3,8 +3,18 @@
 #include "Base.h"
 #include "Base64.h"
 #include "SSL.h"
+#define COM_SMTP_TLS_PORT 587
+#define COM_SMTP_SSL_PORT 465
 #define GMAIL_HOST "smtp.gmail.com"
 #define GMAIL_PORT 587
+#define YAHOO_HOST "smtp.mail.yahoo.com"
+#define YAHOO_PORT 587
+#define OPTONLINE_HOST "mail.optimum.net"
+#define OPTONLINE_PORT 465
+#define AOL_HOST "smtp.aol.com"
+#define AOL_PORT 465
+#define HOTMAIL_HOST "smtp.live.com"
+#define HOTMAIL_PORT 587
 class SmtpClient : public Client {
 private:
 	std::string user;
