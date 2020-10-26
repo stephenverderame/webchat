95f9c08486aec8152c5c44838960b8762b3e3920
Stephen Verderame <stephenverderame@gmail.com>
2020-10-25 23:29:51 -0400

    Gzip for real

--- WebLib2/Coder.cpp
+++ WebLib2/Coder.cpp
@@ -51,96 +51,66 @@ bool GzipCoder::nvi_available() const
 
 int GzipCoder::nvi_encode()
 {
-/*	z_stream zs;
+	z_stream zs;
 	memset(&zs, Z_NULL, sizeof(z_stream));
-	if (str != nullptr) {
-		std::streamsize sizeToProcess = (str->iend_c() - str->icurrent_c() + 1LL);
-//		result.resize(sizeToProcess * 2);
-		zs.avail_in = sizeToProcess;
-		zs.next_in = (Bytef*)str->icurrent_c();
-//		zs.avail_out = result.size();		
-		int err = deflateInit2(&zs, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 16 + MAX_WBITS, 8, Z_DEFAULT_STRATEGY);
-		if (err < Z_OK) return err;
-		result.resize(deflateBound(&zs, sizeToProcess));
-		zs.avail_out = result.size();
-		zs.next_out = (Bytef*)result.data();
+	const char* in = str != nullptr ? str->icurrent_c() : &pimpl->obuffer[0];
+	std::streamsize sizeToProcess = str != nullptr ? str->iend_c() - str->icurrent_c() : pimpl->o;
+	int bufferMultiplier = 1;
+	int err = 0;
+	std::streamsize processedOutput = 0, processedInput = 0;
+	err = deflateInit2(&zs, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 16 + MAX_WBITS, 8, Z_DEFAULT_STRATEGY);
+	if (err < Z_OK) return err;
+	pimpl->ibuffer->resize(deflateBound(&zs, sizeToProcess));
+	do {
+		processedOutput = zs.total_out;
+		processedInput = zs.total_in;
+		if (err == Z_MEM_ERROR || err == Z_BUF_ERROR) {
+			bufferMultiplier *= 2;
+			pimpl->ibuffer->resize(sizeToProcess * bufferMultiplier);
+		}
+		zs.avail_in = sizeToProcess - processedInput;
+		zs.next_in = (Bytef*)in + processedInput;
+		zs.next_out = (Bytef*)pimpl->ibuffer->data() + processedOutput;
+		zs.avail_out = pimpl->ibuffer->size() - processedOutput;
 		err = deflate(&zs, Z_FINISH);
-		deflateEnd(&zs);
-		if (err < Z_OK) return err;
-		pimpl->iend = zs.total_out;
-		setg(result.data(), result.data() + 1, result.data() + zs.total_out);
-	}
-	else {
-		std::streamsize sizeToProcess = pimpl->o;
-		zs.avail_in = sizeToProcess;
-		zs.next_in = (Bytef*)&pimpl->obuffer[0];		
-		int err = deflateInit2(&zs, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 16 + MAX_WBITS, 8, Z_DEFAULT_STRATEGY);
-		if (err < Z_OK) return err;
-		result.resize(deflateBound(&zs, sizeToProcess));
-		zs.avail_out = result.size();
-		zs.next_out = (Bytef*)result.data();
-		err = deflate(&zs, Z_FINISH);
-		deflateEnd(&zs);
-		if (err < Z_OK) return err;
-		pimpl->iend = zs.total_out;
-		setg(result.data(), result.data() + 1, result.data() + zs.total_out);
-		syncOutputBuffer();
-	}
-	return zs.total_out;*/
-	return 0;
+	} while (err == Z_MEM_ERROR || err == Z_BUF_ERROR || (processedInput < sizeToProcess && err > 0));
+	processedOutput = zs.total_out;
+	int e = deflateEnd(&zs);
+	if (err < Z_OK) return err;
+	pimpl->iend = processedOutput;
+	setg(pimpl->ibuffer->data(), pimpl->ibuffer->data(), pimpl->ibuffer->data() + pimpl->iend);
+	return zs.total_out;
 }
 
 int GzipCoder::nvi_decode()
 {
 	z_stream zs;
 	memset(&zs, Z_NULL, sizeof(z_stream));
-	if (str != nullptr) {
-		std::streamsize sizeToProcess = str->iend_c() - str->icurrent_c();
-		int bufferMultiplier = 1;
-		int err = 0;
-		std::streamsize processedOutput = 0, processedInput = 0;
-		err = inflateInit2(&zs, 16 + MAX_WBITS);
-		if (err < Z_OK) return err;
-		pimpl->ibuffer->resize(sizeToProcess * 5);
-		do {
-			processedOutput = zs.total_out;
-			processedInput = zs.total_in;
-			if (err == Z_MEM_ERROR || err == Z_BUF_ERROR) {
-				bufferMultiplier *= 2;
-				pimpl->ibuffer->resize(sizeToProcess * bufferMultiplier);
-			}
-			zs.avail_in = sizeToProcess - processedInput;
-			zs.next_in = (Bytef*)str->icurrent_c() + processedInput;
-			zs.next_out = (Bytef*)pimpl->ibuffer->data() + processedOutput;
-			zs.avail_out = pimpl->ibuffer->size() - processedOutput;
-			err = inflate(&zs, Z_FINISH);			
-		} while (err == Z_MEM_ERROR || err == Z_BUF_ERROR || (processedInput < sizeToProcess && err > 0));
+	const char* in = str != nullptr ? str->icurrent_c() : &pimpl->obuffer[0];
+	std::streamsize sizeToProcess = str != nullptr ? str->iend_c() - str->icurrent_c() : pimpl->o;
+	int bufferMultiplier = 1;
+	int err = 0;
+	std::streamsize processedOutput = 0, processedInput = 0;
+	err = inflateInit2(&zs, 16 + MAX_WBITS);
+	if (err < Z_OK) return err;
+	pimpl->ibuffer->resize(sizeToProcess * 5);
+	do {
 		processedOutput = zs.total_out;
-		int e = inflateEnd(&zs);
-		if (err < Z_OK) return err;
-		pimpl->iend = processedOutput;
-		setg(pimpl->ibuffer->data(), pimpl->ibuffer->data(), pimpl->ibuffer->data() + pimpl->iend);
-	}
-	else {
-/*		std::streamsize sizeToProcess = pimpl->o;
-		int bufferMultiplier = 2;
-		int err = 0;
-		do {
+		processedInput = zs.total_in;
+		if (err == Z_MEM_ERROR || err == Z_BUF_ERROR) {
 			bufferMultiplier *= 2;
-			result.resize(sizeToProcess * bufferMultiplier);
-			zs.avail_in = sizeToProcess;
-			zs.next_in = (Bytef*)&pimpl->obuffer[0];
-			zs.next_out = (Bytef*)result.data();
-			zs.avail_out = result.size();
-			err = inflateInit2(&zs, 16 + MAX_WBITS);
-			if (err < Z_OK) return err;
-			err = inflate(&zs, Z_FINISH);
-			inflateEnd(&zs);
-		} while (err == Z_MEM_ERROR);
-		if (err < Z_OK) return err;
-		pimpl->iend = zs.total_out;
-		setg(result.data(), result.data(), result.data() + zs.total_out);
-		syncOutputBuffer();*/
-	}
+			pimpl->ibuffer->resize(sizeToProcess * bufferMultiplier);
+		}
+		zs.avail_in = sizeToProcess - processedInput;
+		zs.next_in = (Bytef*)in + processedInput;
+		zs.next_out = (Bytef*)pimpl->ibuffer->data() + processedOutput;
+		zs.avail_out = pimpl->ibuffer->size() - processedOutput;
+		err = inflate(&zs, Z_FINISH);
+	} while (err == Z_MEM_ERROR || err == Z_BUF_ERROR || (processedInput < sizeToProcess && err > 0));
+	processedOutput = zs.total_out;
+	int e = inflateEnd(&zs);
+	if (err < Z_OK) return err;
+	pimpl->iend = processedOutput;
+	setg(pimpl->ibuffer->data(), pimpl->ibuffer->data(), pimpl->ibuffer->data() + pimpl->iend);
 	return zs.total_out;
 }
--- WebLib2/StringMap.cpp
+++
@@ -1,1 +0,0 @@
-#include "StringMap.h"
